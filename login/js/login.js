716f04898ee99088d01e69eb4760c81d927b3484
arumi30 <arumidlyt@gmail.com>
2026-05-08 18:52:37 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,7 +1,7 @@
 document.getElementById("loginForm").addEventListener("submit", async function(e) {
     e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
+    const username = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
     const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
